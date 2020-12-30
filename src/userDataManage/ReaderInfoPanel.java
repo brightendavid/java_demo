dd1c5959332df29aaee9cec6301eaaf1f757384c
XZHSL <1318368739@qq.com>
2020-12-30 13:39:09 +0800

    客户数据管理

---
+++ src/userDataManage/ReaderInfoPanel.java
@@ -0,0 +1,130 @@
+package userDataManage;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Map;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.border.LineBorder;
+
+import utils.DBUtils;
+
+public class ReaderInfoPanel extends JPanel {
+
+	private static JLabel ageInfoLabel;
+	private static JLabel emailInfoLabel;
+	private static JLabel telInfoLabel;
+
+	/**
+	 * Create the panel.
+	 */
+	public ReaderInfoPanel() {
+		setBorder(new LineBorder(new Color(0, 0, 0)));
+
+		int number = ReaderEntrance.readerNumber;
+
+		Map<String, Object> readerInfo = DBUtils.getStudentInfo(number);
+
+		setLayout(null);
+
+		JLabel titleLabel = new JLabel("ª∂”≠Ω¯»Î±®øØ∂©‘ƒπ‹¿ÌœµÕ≥");
+		titleLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		titleLabel.setFont(new Font("Œ¢»Ì—≈∫⁄", Font.PLAIN, 25));
+		titleLabel.setBounds(111, 23, 319, 40);
+		add(titleLabel);
+
+		JLabel nameLabel = new JLabel("–’√˚£∫");
+		nameLabel.setFont(new Font("Œ¢»Ì—≈∫⁄", Font.PLAIN, 20));
+		nameLabel.setBounds(50, 85, 60, 40);
+		add(nameLabel);
+
+		/*JLabel nunberLabel = new JLabel("±‡∫≈£∫");
+		nunberLabel.setFont(new Font("Œ¢»Ì—≈∫⁄", Font.PLAIN, 20));
+		nunberLabel.setBounds(50, 160, 60, 40);
+		add(nunberLabel);*/
+
+		JLabel sexLabel = new JLabel("–‘±£∫");
+		sexLabel.setFont(new Font("Œ¢»Ì—≈∫⁄", Font.PLAIN, 20));
+		sexLabel.setBounds(191, 85, 60, 40);
+		add(sexLabel);
+
+		JLabel ageLabel = new JLabel("ƒÍ¡‰£∫");
+		ageLabel.setFont(new Font("Œ¢»Ì—≈∫⁄", Font.PLAIN, 20));
+		ageLabel.setBounds(190, 160, 60, 40);
+		add(ageLabel);
+
+		JLabel telLabel = new JLabel("µÁª∞£∫");
+		telLabel.setFont(new Font("Œ¢»Ì—≈∫⁄", Font.PLAIN, 20));
+		telLabel.setBounds(319, 85, 60, 40);
+		add(telLabel);
+
+		JLabel emailLabel = new JLabel("” œ‰£∫");
+		emailLabel.setFont(new Font("Œ¢»Ì—≈∫⁄", Font.PLAIN, 20));
+		emailLabel.setBounds(319, 160, 60, 40);
+		add(emailLabel);
+
+		JLabel nameInfoLabel = new JLabel(String.valueOf(readerInfo.get("name")));
+		nameInfoLabel.setFont(new Font("Œ¢»Ì—≈∫⁄", Font.PLAIN, 20));
+		nameInfoLabel.setBounds(104, 85, 77, 40);
+		add(nameInfoLabel);
+
+		JLabel sexInfoLabel = new JLabel(String.valueOf(readerInfo.get("sex")));
+		sexInfoLabel.setFont(new Font("Œ¢»Ì—≈∫⁄", Font.PLAIN, 20));
+		sexInfoLabel.setBounds(245, 85, 60, 40);
+		add(sexInfoLabel);
+
+		telInfoLabel = new JLabel(String.valueOf(readerInfo.get("telephone")));
+		telInfoLabel.setFont(new Font("Œ¢»Ì—≈∫⁄", Font.PLAIN, 16));
+		telInfoLabel.setBounds(377, 85, 129, 40);
+		add(telInfoLabel);
+
+		/*JLabel numberInfoLabel = new JLabel(String.valueOf(readerInfo.get("number")));
+		numberInfoLabel.setFont(new Font("Œ¢»Ì—≈∫⁄", Font.PLAIN, 20));
+		numberInfoLabel.setBounds(105, 160, 75, 40);
+		add(numberInfoLabel);*/
+
+		ageInfoLabel = new JLabel(String.valueOf(readerInfo.get("age")));
+		ageInfoLabel.setFont(new Font("Œ¢»Ì—≈∫⁄", Font.PLAIN, 20));
+		ageInfoLabel.setBounds(245, 160, 60, 40);
+		add(ageInfoLabel);
+
+		emailInfoLabel = new JLabel(String.valueOf(readerInfo.get("email")));
+		emailInfoLabel.setFont(new Font("Œ¢»Ì—≈∫⁄", Font.PLAIN, 14));
+		emailInfoLabel.setBounds(377, 160, 129, 40);
+		add(emailInfoLabel);
+
+		JButton infoModifyButton = new JButton("–ﬁ∏ƒ∏ˆ»À–≈œ¢");
+		infoModifyButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				new ReaderInfoModifier();
+			}
+		});
+		infoModifyButton.setFont(new Font("Œ¢»Ì—≈∫⁄", Font.PLAIN, 20));
+		infoModifyButton.setBounds(88, 233, 162, 50);
+		add(infoModifyButton);
+
+		JButton passwordModifyButton = new JButton("–ﬁ∏ƒ√‹¬Î");
+		passwordModifyButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				new ReaderPasswordModifier();
+			}
+		});
+		passwordModifyButton.setFont(new Font("Œ¢»Ì—≈∫⁄", Font.PLAIN, 20));
+		passwordModifyButton.setBounds(338, 233, 114, 50);
+		add(passwordModifyButton);
+
+	}
+
+	public static void updatePanel() {
+		int number = ReaderEntrance.readerNumber;
+		Map<String, Object> readerInfo = DBUtils.getStudentInfo(number);
+		telInfoLabel.setText(String.valueOf(readerInfo.get("telephone")));
+		ageInfoLabel.setText(String.valueOf(readerInfo.get("age")));
+		emailInfoLabel.setText(String.valueOf(readerInfo.get("email")));
+	}
+}
---
+++ src/userDataManage/ReaderPasswordModifier.java
@@ -0,0 +1,144 @@
+package userDataManage;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.border.EmptyBorder;
+
+import User.ReaderEntrance;
+import utils.DBUtils;
+
+public class ReaderPasswordModifier {
+
+	private final JDialog dialog = new JDialog();
+	private final JPanel contentPanel = new JPanel();
+	private final JPanel infoPanel = new JPanel();
+	private final JLabel oldPasswordLabel = new JLabel("原密码：");
+	private final JPasswordField oldPasswordTextField = new JPasswordField();
+	private final JLabel newPasswordLabel = new JLabel("新密码：");
+	private final JPasswordField newPasswordTextField = new JPasswordField();
+	private final JLabel verifyPasswordLabel = new JLabel("密码确认：");
+	private final JPasswordField verifyPasswordTextField = new JPasswordField();
+
+	/**
+	 * Create the dialog.
+	 */
+	public ReaderPasswordModifier() {
+		dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+		dialog.setVisible(true);
+		dialog.setBounds(100, 100, 300, 300);
+		dialog.setContentPane(contentPanel);
+		contentPanel.setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPanel.add(infoPanel, BorderLayout.CENTER);
+		infoPanel.setLayout(null);
+		{
+			oldPasswordLabel.setFont(new Font("微软雅黑", Font.PLAIN, 20));
+			oldPasswordLabel.setBounds(36, 25, 105, 40);
+			infoPanel.add(oldPasswordLabel);
+		}
+		{
+			oldPasswordTextField.setFont(new Font("微软雅黑", Font.PLAIN, 20));
+			oldPasswordTextField.setBounds(151, 25, 90, 40);
+			infoPanel.add(oldPasswordTextField);
+		}
+		{
+			newPasswordLabel.setFont(new Font("微软雅黑", Font.PLAIN, 20));
+			newPasswordLabel.setBounds(36, 90, 105, 40);
+			infoPanel.add(newPasswordLabel);
+		}
+		{
+			newPasswordTextField.setFont(new Font("微软雅黑", Font.PLAIN, 20));
+			newPasswordTextField.setBounds(151, 90, 90, 40);
+			infoPanel.add(newPasswordTextField);
+		}
+		{
+			verifyPasswordLabel.setFont(new Font("微软雅黑", Font.PLAIN, 20));
+			verifyPasswordLabel.setBounds(36, 155, 105, 40);
+			infoPanel.add(verifyPasswordLabel);
+		}
+		{
+			verifyPasswordTextField.setFont(new Font("微软雅黑", Font.PLAIN, 20));
+			verifyPasswordTextField.setBounds(151, 155, 90, 40);
+			infoPanel.add(verifyPasswordTextField);
+		}
+
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			contentPanel.add(buttonPane, BorderLayout.SOUTH);
+			{
+				JButton okButton = new JButton("OK");
+				okButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						String oldPassword = oldPasswordTextField.getText();
+						String newPassword = newPasswordTextField.getText();
+						String verifyPassword = verifyPasswordTextField.getText();
+
+						if (oldPassword.equals("") || newPassword.equals("") || verifyPassword.equals("")) {
+							JOptionPane.showMessageDialog(null, "原密码、新密码、密码确认均不能为空，请继续输入", "通知",
+									JOptionPane.INFORMATION_MESSAGE);
+							return;
+						}
+
+						if (!newPassword.equals(verifyPassword)) {
+							JOptionPane.showMessageDialog(null, "新密码、密码确认必须相同，请重新输入", "通知",
+									JOptionPane.INFORMATION_MESSAGE);
+							return;
+						}
+						if (newPassword.length() < 6) {
+							JOptionPane.showMessageDialog(null, "新密码长度须不少于6位，请重新输入", "通知",
+									JOptionPane.INFORMATION_MESSAGE);
+							return;
+						}
+
+						int n = JOptionPane.showConfirmDialog(null, "确认修改吗?", "提示", JOptionPane.YES_NO_OPTION);
+						if (n == JOptionPane.YES_OPTION) {
+							int number = ReaderEntrance.readerNumber;
+							String nowPassword = DBUtils.getNowPassword(number);
+							if (!nowPassword.equals(oldPassword)) {
+								JOptionPane.showMessageDialog(null, "密码校验失败，请重新输入", "提示",
+										JOptionPane.INFORMATION_MESSAGE);
+								return;
+							}
+
+							if (DBUtils.updateReaderPassword(number, newPassword)) {
+								JOptionPane.showMessageDialog(null, "修改成功", "提示", JOptionPane.INFORMATION_MESSAGE);
+								dialog.dispose();
+							} else {
+								JOptionPane.showMessageDialog(null, "修改失败，请重试", "提示", JOptionPane.INFORMATION_MESSAGE);
+								return;
+							}
+
+						} else if (n == JOptionPane.NO_OPTION) {
+							return;
+						}
+					}
+				});
+				okButton.setActionCommand("OK");
+				buttonPane.add(okButton);
+				dialog.getRootPane().setDefaultButton(okButton);
+			}
+			{
+				JButton cancelButton = new JButton("Cancel");
+				cancelButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						dialog.dispose();
+					}
+				});
+				cancelButton.setActionCommand("Cancel");
+				buttonPane.add(cancelButton);
+			}
+		}
+	}
+
+}
